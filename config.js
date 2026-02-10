fcd647e0d48642fc2690ad76d90565bd3aa1f154
Pablo Rodriguez <arnejado@hotmail.com>
2026-02-10 13:58:58 +0100

    subido a netlify

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 // 1. CONSTANTES Y SELECTORES
 export const misEnlaces = [
     { nombre: "Gaio", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
-    { nombre: "GitHub", url: "https://github.com", desc: "Repositorios", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
+    { nombre: "GitHub", url: "https://github.com/login", desc: "Repositorios", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
+    { nombre: "Netlify", url: "https://netlify.com/login/email", desc: "Hosting y despliegue", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
     { nombre: "Gemini", url: "https://gemini.google.com", desc: "Asistente AI", categoria: "ia", tags: ["AI", "asistente", "google"] },
     { nombre: "ChatGPT", url: "https://chatgpt.com", desc: "Asistente AI", categoria: "ia", tags: ["AI", "asistente", "openai"] },
     { nombre: "CSSGridGenerator", url: "https://cssgrid-generator.netlify.app/", desc: "Generador CSS Grid", categoria: "desarrollo", tags: ["css", "grid", "generador"] },
     { nombre: "Canva", url: "https://www.canva.com/", desc: "Diseño gráfico", categoria: "diseño", tags: ["diseño", "gráfico", "canva"] },
+    { nombre: "Tinkercad", url: "https://www.tinkercad.com/", desc: "Diseño 3D", categoria: "diseño", tags: ["diseño", "3D", "tinkercad"] },
 ];
