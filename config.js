40c52aea7e42f1a372fa0999bff037a37fa8312e
Pablo Rodriguez <114185307+arnejado@users.noreply.github.com>
2026-02-11 21:25:46 +0100

    prueba de un nuevo elemento giro superado

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 1. CONSTANTES Y SELECTORES
 export const misEnlaces = [
-    { nombre: "Gaio", tipo: "principal", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", nota: "aquí va la nota", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
+    { nombre: "Gaio", tipo: "principal", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", nota: "traductor de la xunta de galicia", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
     { nombre: "GitHub", tipo: "principal", url: "https://github.com/login", desc: "Repositorios", nota: "aquí va la nota", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
     { nombre: "Netlify", tipo: "principal", url: "https://netlify.com/login/email", desc: "Hosting y despliegue", nota: "aquí va la nota", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
     { nombre: "Gemini", tipo: "principal", url: "https://gemini.google.com", desc: "Asistente AI", nota: "aquí va la nota", categoria: "ia", tags: ["AI", "asistente", "google"] },
@@ -9,5 +9,6 @@ export const misEnlaces = [
     { nombre: "Canva", tipo: "principal", url: "https://www.canva.com/", desc: "Diseño gráfico", nota: "aquí va la nota", categoria: "diseño", tags: ["diseño", "gráfico", "canva"] },
     { nombre: "Tinkercad", tipo: "principal", url: "https://www.tinkercad.com/", desc: "Diseño 3D", nota: "aquí va la nota", categoria: "diseño", tags: ["diseño", "3D", "tinkercad"] },
     { nombre: "NeonTech", tipo: "principal", url: "https://neontech.com/", desc: "Bases de Datos", nota: "aquí va la nota", categoria: "desarrollo", tags: ["tecnología", "bases de datos", "postgreSQL", "gadgets", "neontech"] },
-    { nombre: "Animista", tipo: "archivo", url: "https://animista.net/", desc: "Animaciones CSS", nota: "aquí va la nota", categoria: "diseño", tags: ["animación", "css", "animista"] },
+    { nombre: "Animista", tipo: "archivo", url: "https://animista.net/", desc: "Animaciones CSS", nota: "miles de efectos css", categoria: "diseño", tags: ["animación", "css", "animista"] },
+    { nombre: "htmlrev", tipo: "archivo", url: "https://htmlrev.com/", desc: "Plantillas Html", nota: "miles de plantillas para un monton de framewors (de midudev)", categoria: "diseño", tags: ["plantillas", "html", "astro", "talwind"]},
 ];
