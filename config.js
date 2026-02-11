bede44448fa35031abfd8fa0711b2175d9de8ae0
Pablo Rodriguez <arnejado@hotmail.com>
2026-02-11 14:55:43 +0100

    añadida key nota

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 // 1. CONSTANTES Y SELECTORES
 export const misEnlaces = [
-    { nombre: "Gaio", tipo: "principal", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
-    { nombre: "GitHub", tipo: "principal", url: "https://github.com/login", desc: "Repositorios", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
-    { nombre: "Netlify", tipo: "principal", url: "https://netlify.com/login/email", desc: "Hosting y despliegue", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
-    { nombre: "Gemini", tipo: "principal", url: "https://gemini.google.com", desc: "Asistente AI", categoria: "ia", tags: ["AI", "asistente", "google"] },
-    { nombre: "ChatGPT", tipo: "principal", url: "https://chatgpt.com", desc: "Asistente AI", categoria: "ia", tags: ["AI", "asistente", "openai"] },
-    { nombre: "CSSGridGenerator", tipo: "archivo", url: "https://cssgrid-generator.netlify.app/", desc: "Generador CSS Grid", categoria: "desarrollo", tags: ["css", "grid", "generador"] },
-    { nombre: "Canva", tipo: "principal", url: "https://www.canva.com/", desc: "Diseño gráfico", categoria: "diseño", tags: ["diseño", "gráfico", "canva"] },
-    { nombre: "Tinkercad", tipo: "principal", url: "https://www.tinkercad.com/", desc: "Diseño 3D", categoria: "diseño", tags: ["diseño", "3D", "tinkercad"] },
-    { nombre: "NeonTech", tipo: "principal", url: "https://neontech.com/", desc: "Bases de Datos", categoria: "desarrollo", tags: ["tecnología", "bases de datos", "postgreSQL", "gadgets", "neontech"] },
+    { nombre: "Gaio", tipo: "principal", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", nota: "aquí va la nota", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
+    { nombre: "GitHub", tipo: "principal", url: "https://github.com/login", desc: "Repositorios", nota: "aquí va la nota", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
+    { nombre: "Netlify", tipo: "principal", url: "https://netlify.com/login/email", desc: "Hosting y despliegue", nota: "aquí va la nota", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
+    { nombre: "Gemini", tipo: "principal", url: "https://gemini.google.com", desc: "Asistente AI", nota: "aquí va la nota", categoria: "ia", tags: ["AI", "asistente", "google"] },
+    { nombre: "ChatGPT", tipo: "principal", url: "https://chatgpt.com", desc: "Asistente AI", nota: "aquí va la nota", categoria: "ia", tags: ["AI", "asistente", "openai"] },
+    { nombre: "CSSGridGenerator", tipo: "archivo", url: "https://cssgrid-generator.netlify.app/", desc: "Generador CSS Grid", nota: "aquí va la nota", categoria: "desarrollo", tags: ["css", "grid", "generador"] },
+    { nombre: "Canva", tipo: "principal", url: "https://www.canva.com/", desc: "Diseño gráfico", nota: "aquí va la nota", categoria: "diseño", tags: ["diseño", "gráfico", "canva"] },
+    { nombre: "Tinkercad", tipo: "principal", url: "https://www.tinkercad.com/", desc: "Diseño 3D", nota: "aquí va la nota", categoria: "diseño", tags: ["diseño", "3D", "tinkercad"] },
+    { nombre: "NeonTech", tipo: "principal", url: "https://neontech.com/", desc: "Bases de Datos", nota: "aquí va la nota", categoria: "desarrollo", tags: ["tecnología", "bases de datos", "postgreSQL", "gadgets", "neontech"] },
+    { nombre: "Animista", tipo: "archivo", url: "https://animista.net/", desc: "Animaciones CSS", nota: "aquí va la nota", categoria: "diseño", tags: ["animación", "css", "animista"] },
 ];
