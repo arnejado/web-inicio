b2fe625203600bb9467238dd8e5310fb428f3810
Pablo Rodriguez <arnejado@hotmail.com>
2026-02-12 08:49:55 +0100

    arreglado el giro

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 export const misEnlaces = [
     { nombre: "Gaio", tipo: "principal", url: "https://tradutorgaio.xunta.gal/TradutorPublico/traducir/index", desc: "traductor xunta", nota: "traductor de la xunta de galicia", categoria: "traductores", tags: ["traductor", "xunta", "galego"] },
     { nombre: "GitHub", tipo: "principal", url: "https://github.com/login", desc: "Repositorios", nota: "aquí va la nota", categoria: "desarrollo", tags: ["codigo", "repositorios", "desarrollo"] },
-    { nombre: "Netlify", tipo: "principal", url: "https://netlify.com/login/email", desc: "Hosting y despliegue", nota: "aquí va la nota", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
+    { nombre: "Netlify", tipo: "principal", url: "https://netlify.com/login/email", desc: "Hosting", nota: "aquí va la nota", categoria: "desarrollo", tags: ["hosting", "despliegue", "netlify"] },
     { nombre: "Gemini", tipo: "principal", url: "https://gemini.google.com", desc: "Asistente AI", nota: "aquí va la nota", categoria: "ia", tags: ["AI", "asistente", "google"] },
     { nombre: "ChatGPT", tipo: "principal", url: "https://chatgpt.com", desc: "Asistente AI", nota: "aquí va la nota", categoria: "ia", tags: ["AI", "asistente", "openai"] },
     { nombre: "CSSGridGenerator", tipo: "archivo", url: "https://cssgrid-generator.netlify.app/", desc: "Generador CSS Grid", nota: "aquí va la nota", categoria: "desarrollo", tags: ["css", "grid", "generador"] },
